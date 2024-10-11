23457820a715426358fd0673f319d4e754e18795
Faizan-Ansari1000 <ansarizada70@gmail.com>
2024-10-11 03:39:22 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import QuizApp from './QuizApp/QuizApp';
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div>
+      <QuizApp />
+    </div>
+  )
+}
+
+
+
+export default App;
